028614a77b288f7dc2a752b14222dc9a99b7a8fc
Michał Budzyński <43907647+michalbudzynski@users.noreply.github.com>
2019-10-07 20:29:02 +0200

    Add files via upload

--- footer_link_size_test.py
+++ footer_link_size_test.py
@@ -11,6 +11,7 @@ class LoginTest(unittest.TestCase):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
         self.driver = webdriver.Chrome()
